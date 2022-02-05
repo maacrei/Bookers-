66be8b090503464d920948eedafa943e4c9e1ebe
EC2 Default User <ec2-user@ip-172-31-44-157.ec2.internal>
2022-02-05 08:39:49 +0000

    [Finish]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,20 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: "Book was successfully created."
+    else
+      render :index
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -22,24 +26,25 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "Book was successfully updated."
+    else
+      render :edit
+    end
   end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    redirect_to '/books', notice: "Book was successfully destroyed."
   end
   
   
   private
   
   def book_params
-   params.permit(:title, :body)
-  # 要確認　カリキュラムと記述が違うから
-  # params.require(:book).permit(:title, :body)のrequire(:book)を削除するとエラーが解消された
+   params.require(:book).permit(:title, :body)
   end
   
 end
